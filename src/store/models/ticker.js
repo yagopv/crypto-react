cb0de0e3768d710cb436b2ad4b64e9812e00ea72
Yago Pérez Vázquez <yagoperez@outlook.com>
2018-08-28 12:51:41 +0200

    [ADD] Get tickers method

--- src/store/models/ticker.js
+++ src/store/models/ticker.js
@@ -1,14 +1,51 @@
+import mapKeys from 'lodash/mapKeys';
+import { getTickers, getCoinList } from 'services/http/api';
+
 export default {
-  state: {},
+  state: {
+    byId: {},
+    global: null,
+    sortKey: { rank: 'Number' },
+    sortOrder: 'asc',
+    searchValue: ''
+  },
   reducers: {
-    increment(state, payload) {
-      return state + payload;
+    setTickers(state, payload) {
+      return {
+        ...state,
+        byId: mapKeys(payload, ticker => ticker.id)
+      };
     }
   },
   effects: dispatch => ({
-    async incrementAsync(payload, rootState) {
-      await new Promise(resolve => setTimeout(resolve, 1000));
-      dispatch.count.increment(payload);
+    async getTickers(payload, rootState) {
+      try {
+        const tickers = getTickers();
+        const coinList = getCoinList();
+        const tickersResult = await tickers;
+        const coinListResult = await coinList;
+
+        if (coinListResult.data['Response'] === 'Error') {
+          throw new Error(coinListResult.data['ErrorsSummary']);
+        }
+
+        if (
+          tickersResult.data &&
+          coinListResult.data &&
+          coinListResult.data['Data']
+        ) {
+          tickersResult.data.forEach(ticker => {
+            ticker.meta = coinListResult.data['Data'][ticker.symbol];
+
+            // IOTA ticker is different in coinmarketcap than in cryptocompare
+            if (ticker.symbol === 'MIOTA') {
+              ticker.symbol = 'IOT';
+              ticker.meta = coinListResult.data['Data']['IOT'];
+            }
+          });
+        }
+        dispatch.ticker.setTickers(tickersResult);
+      } catch (error) {}
     }
   })
 };
